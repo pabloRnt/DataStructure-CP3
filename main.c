93204a668a372074e4bd3772340e33062ec0643d
Dustyluc123 <lucasbrsantana@gmail.com>
2026-05-15 08:44:25 -0300

    criação do arquivo main.c || criação do menu com switch dentro do arquivo main.

---
+++ main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+
+
+int main() {
+    int opcao = 0;
+
+    
+    while(opcao != 5) {
+        printf("\n========================\n");
+        printf(" CAIXA ELETRONICO \n");
+        printf("========================\n");
+        printf("1 - Consultar Saldo\n");
+        printf("2 - Realizar Saque\n");
+        printf("3 - Realizar Deposito\n");
+        printf("0 - Sair\n");
+        printf("========================\n");
+        printf("Escolha uma opcao: ");
+        
+        // Lê a escolha do usuário
+        scanf("%d", &opcao);
+
+        // Estrutura de decisão baseada na variável 'opcao'
+        switch(opcao) {
+
+            case 1:
+               
+                printf("Saldo: R$ 1000,00\n");
+                    break;
+
+            case 2:
+                printf("Digite o valor do saque: ");
+                float saque;
+                scanf("%f", &saque);
+                if (saque > 1000) {
+                    printf("Saldo insuficiente!\n");
+                } else {
+                    printf("Saque realizado com sucesso! Novo saldo: R$ %.2f\n", 1000 - saque);
+                }
+                break;
+            case 3:
+                printf("Digite o valor do deposito: ");
+                float deposito;
+                scanf("%f", &deposito);
+                printf("Deposito realizado com sucesso! Novo saldo: R$ %.2f\n", 1000 + deposito);
+                break;
+                
+            case 0: { 
+                 // Finaliza o loop while 
+                printf("Saindo...\n");
+                break;
+                
+            }
+
+            default:
+                // Executado caso o usuário digite um número que não esteja no menu
+                printf("Opcao invalida!\n");
+        }
+    }
+
+    return 0; // Encerra o programa com sucesso
+}
